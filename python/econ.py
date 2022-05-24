472abac3012731ad615292e15fca7c48cff39f98
ricky <cc2297.fn@gmail.com>
2022-05-24 15:22:38 +0800

    backend server

---
+++ python/econ.py
@@ -0,0 +1,15 @@
+
+from binance.spot import Spot 
+
+
+def klines(symbol, interval, num = 1000):
+    assert 0 < num and num <= 1000
+    client = Spot()
+    return client.klines(symbol, interval, limit=num)
+
+## Economic Indicator Here ~
+
+
+if __name__ == "__main__":
+    # Test Section
+    print( klines("ETHUSDT", '1m', 100) )
---
+++ readme.md
@@ -0,0 +1,14 @@
+# 2022 NTU dApp: 韭菜王補想肚子餓
+
+## Members
++ 資工碩一 r10922064 邱詠智
++ 資工碩一 r10922146 黃資傑
++ 網媒碩一 r10944055 張禾姈
++ 資工碩一 r10922104 賴昱宏
++ 資工碩一 r10922144 林辰臻
+
+## Backend Server
+1. Setup the (HOST, PORT) in `CCserver.py`. defaut is (localhost, 9999)
+2. Running the server `$ python3 CCserver.py`
+
+
